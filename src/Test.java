ea65c0a70e8b7e4236d1895d8f565abe91fbc7b6
Harit Patwa <patwaharit111@gmail.com>
2021-08-02 07:36:40 +0530

    added insert into TableUtil

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,18 @@
+import Utilities.Context;
+import main.java.queries.TableUtils;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
 public class Test {
 
-  public static void main(String [] args)
-  {
+  public static void main(String [] args) throws FileNotFoundException {
+    Context.setDbName("Database1");
+    HashMap<String,String> hmp = new HashMap<>();
+    hmp.put("empId","ep9");
+    hmp.put("empName","abc");
+    hmp.put("empAddress","New York");
+    hmp.put("empSalary","");
+    TableUtils.insertRow("Database1","employee",hmp);
   }
 }
--- src/main/java/queries/TableUtils.java
+++ src/main/java/queries/TableUtils.java
@@ -4,8 +4,11 @@ import Utilities.Context;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TableUtils {
@@ -54,5 +57,38 @@ public class TableUtils {
       return totalColumn;
     }
     return null;
+  }
+  public static boolean insertRow(String dbName, String tableName, HashMap<String, String> insertData) throws FileNotFoundException {
+
+    HashMap<String, ArrayList<String>> tableData = getColumns(dbName, tableName);
+    for(Map.Entry<String, ArrayList<String>> entry: tableData.entrySet()) {
+      entry.getValue().add(insertData.get(entry.getKey()));
+    }
+    insertTableData(dbName,tableName,tableData);
+    return true;
+  }
+  public static void insertTableData(String dbName, String tableName,HashMap<String,ArrayList<String>> tableData)
+  {
+    if (Context.setDbName(dbName)&&Context.isTableExist(tableName)){
+      try {
+        FileWriter myWriter = new FileWriter("Databases/"+dbName+"/"+tableName+".txt");
+
+        for(Map.Entry<String, ArrayList<String>> entry: tableData.entrySet()) {
+         myWriter.write(entry.getKey()+"|");
+         for(int i=0;i<entry.getValue().size();i++)
+         {
+           myWriter.write(entry.getValue().get(i)+"|");
+         }
+         myWriter.write("\n");
+        }
+        myWriter.close();
+
+      } catch (IOException e) {
+
+        e.printStackTrace();
+      }
+
+
+    }
   }
 }
